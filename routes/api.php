d59aab067beb3bb890663d622f53f0e61b40fc17
Meera Tank <meera.tank@cloudester.com>
2024-03-23 18:23:49 +0530

    initial register and login functionality added for user

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller(UserController::class)->group(function() {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login')->name('login');
+});
+
